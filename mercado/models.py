ba834b44c5b97a69daf65d2df2cd4e7fe6f8e88e
paulo2k21 <paulomarques.ufsb@gmail.com>
2024-03-26 12:24:03 -0300

    Atualização botão detalhe, ajuste slide, criação paginas; Sobre e Contato. otimização da pagina produto unico.

--- mercado/models.py
+++ mercado/models.py
@@ -6,7 +6,7 @@ class Produtos (models.Model):
     nome = models.CharField(max_length=250)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.CharField(max_length=250)
-    descricao = models.CharField(max_length=250)
+    descricao = models.CharField(max_length=1500)
     imagem = models.CharField(max_length=250)
     estado_produto = models.ForeignKey("Estado_Produto", on_delete=models.CASCADE, null=True)
     def __str__(self):
