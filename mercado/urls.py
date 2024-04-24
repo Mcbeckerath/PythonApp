48da7a58d6db1fc020b98b36930b0530476afc21
Matheus Beckerath <matheus.beckerath@hotmail.com>
2024-04-24 15:37:22 -0300

    Form produto

--- mercado/urls.py
+++ mercado/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('produto/<id_produto>', views.produto, name='produto'),
     path('sobre/', views.sobre, name='sobre'),
-    path('form_produto', views.form_produto, name ='form_produto')
+    path('contato/', views.contato, name='contato'),
+    path('form_produto/', views.form_produto, name ='form_produto')
 ]
--- mercado/views.py
+++ mercado/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from mercado.models import Produtos
-from .forms import  ProdutosForm
+from mercado.forms import  ProdutosForm
 # Create your views here.
 
 def home (request):
@@ -35,16 +35,12 @@ def sobre (request):
 
 def form_produto(request):
     """Adiciona um novo produto"""
-    if request.method != 'POST':
-        #Nenhum dado submetido; cria um formulario em branco
-        form = ProdutosForm()
-    else:
-        #Dados de POST submetidos; processa dados
-        form = ProdutosForm(request.POST)
-        if form.is_valid():
+    form = ProdutosForm(request.POST)
+   
+    if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('produto'))
+    return HttpResponseRedirect(reverse('form_produto'))
 
 
     context = {'form': form}
-    return render (request, 'mercado/form_produto.html', context)
+    return render (request, 'form_produto.html', context)
